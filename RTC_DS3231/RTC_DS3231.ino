cee9b3dd17886234a2931c6c019cc93495bd086e
Gertjan van Heertum <git@heertum.net>
2020-03-22 14:48:16 +0100

    Update RTC_DS3231.ino
    Reordering of the code

--- RTC_DS3231/RTC_DS3231.ino
+++ RTC_DS3231/RTC_DS3231.ino
@@ -4,42 +4,50 @@
 RTClib RTC;
 DS3231 Clock;
 
-void setup () {
+// Connect 
+// SDA -> A4
+// SCL -> A5
+
+void setup () 
+{
   Wire.begin();
   Serial.begin(9600);
   
-    /*Clock.setClockMode(false);  
-    Clock.setYear(20);
-    Clock.setMonth(3);
-    Clock.setDate(20);
-    Clock.setHour(16);
-    Clock.setMinute(55);
-    */
-    
+  //setupTime();   
+  
 }
 
-void loop () {
+void setupTime()
+{
+  Clock.setClockMode(false);  
+  Clock.setYear(20);
+  Clock.setMonth(3);
+  Clock.setDate(20);
+  Clock.setHour(16);
+  Clock.setMinute(55);
+}
+
+void loop () 
+{  
+  delay(1000);
+  DateTime now = RTC.now();
   
-    delay(1000);
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(' ');
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
   
-    DateTime now = RTC.now();
-    
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
+  Serial.print(" since midnight 1/1/1970 = ");
+  Serial.print(now.unixtime());
+  Serial.print("s = ");
+  Serial.print(now.unixtime() / 86400L);
+  Serial.println("d");
 }
