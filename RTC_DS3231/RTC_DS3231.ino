abc8a91800552dbf32b6e091138fd50afe811124
Gertjan van Heertum <git@heertum.net>
2020-03-22 14:34:17 +0100

    Create RTC_DS3231.ino
    RTC Test

---
+++ RTC_DS3231/RTC_DS3231.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+#include <DS3231.h>
+
+RTClib RTC;
+DS3231 Clock;
+
+void setup () {
+  Wire.begin();
+  Serial.begin(9600);
+  
+    /*Clock.setClockMode(false);  
+    Clock.setYear(20);
+    Clock.setMonth(3);
+    Clock.setDate(20);
+    Clock.setHour(16);
+    Clock.setMinute(55);
+    */
+    
+}
+
+void loop () {
+  
+    delay(1000);
+  
+    DateTime now = RTC.now();
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+}
